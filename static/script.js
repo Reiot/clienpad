af5db8b3e215656cd8c78538f5670bc94780ff64
Reiot <Reiot94@gmail.com>
2011-06-18 20:21:49 +0900

    사진 게시판 more 구현 개수 초과할 경우 기존꺼 삭제

--- static/script.js
+++ static/script.js
@@ -1,22 +1,51 @@
+function render_post(board, post){
+	var html = '<li>'
+	+ post.author
+	+ '<a href="/' + board + '/' + post.id + '">' 
+	+ post.title 
+	+ '<span class="ui-li-aside ui-li-desc">' + post.publish_time_short + '</span>'
+	+ '</a>';
+	if(post.comments>0){
+		html += '<span class="ui-li-count">' + post.comments + '</span>';
+	}					
+	html += '</li>';					
+	return html;
+}
+
+function render_image_post(board, post){
+	var html = 
+	'<li data-role="list-divider">' +
+	    post.author +
+	    '<h2>' + post.title + '</h2>' +
+	'</li>' +
+	'<li>' +
+		'<div>' + post.content + '</div>';
+		
+	if(post.comments.length){
+		html += '<ul data-role="listview" data-inset="true">';
+		var i;
+		for(i = 0 ; i < post.comments.length ; i ++ ){
+		    var comment = post.comments[i];
+			html += '<li>' + comment.author + comment.content + '</li>';
+		}
+		html += '</ul>';
+	}
+	html += '</li>';
+	return html;
+}
+
 function add_post(board, $posts, data){
 	var i;
 	for( i = 0 ; i < data.length ; i ++ ){
-		var post = data[i];
-		var li = '<li>'
-		+ post.author
-		+ '<a href="/' + board + '/' + post.id + '">' 
-		+ post.title 
-		+ '<span class="ui-li-aside ui-li-desc">' + post.publish_time_short + '</span>'
-		+ '</a>';
-		if(post.comments>0){
-			li += '<span class="ui-li-count">' + post.comments + '</span>';
-		}					
-		li += '</li>';					
-		
+		var li;
+		if(board==="image"){
+		    li = render_image_post(board, data[i]);
+		}else{
+            li = render_post(board, data[i]);
+		}
 		$posts.append(li);
 	}
-	$posts.listview('refresh');
-	
+	$posts.listview('refresh');	
 }
 
 $(function(){
@@ -55,25 +84,15 @@ $(function(){
 		$.getJSON("/"+board+"/page"+nextPage, { format: "json" })
 			.success(function(response){
 				console.log('response',response);
-				
+				add_post(board, $posts, response.posts);
 				$button.data('next-page', response.next_page);
-				var i;
-				for( i = 0 ; i < response.posts.length ; i ++ ){
-					var post = response.posts[i];
-					var li = '<li>'
-					+ post.author
-					+ '<a href="/' + board + '/' + post.id + '">' 
-					+ post.title 
-					+ '<span class="ui-li-aside ui-li-desc">' + post.publish_time_short + '</span>'
-					+ '</a>';
-					if(post.comments>0){
-						li += '<span class="ui-li-count">' + post.comments + '</span>';
-					}					
-					li += '</li>';					
-					
-					$posts.append(li);
+				
+				if(board==="image"){
+				    var $li = $posts.find("li");
+				    if($li.size() > 16){
+				        $posts.find("li:lt(4)").remove();
+				    }
 				}
-				$posts.listview('refresh');
 			})
 			.complete(function(){
 				$.mobile.pageLoading(true);
